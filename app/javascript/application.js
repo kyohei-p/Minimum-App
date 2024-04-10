2e9f126bcd357aef1d0147cdfe52ba8f5f7feacc
kyohei-p <ysdkyh11@gmail.com>
2024-04-11 06:23:22 +0900

    jqueryのコメントアウト（JSの実装も検討）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,22 @@
-import $ from 'jquery';
-import "jquery-ujs";
-import "@hotwired/turbo-rails";
-import "controllers";
+// import $ from 'jquery';
+// import "@hotwired/turbo-rails";
+// import "controllers";
 
-$(function() {
-  $('.flash-message').fadeOut(4000);
-  window.location.reload();
-});
+// turboでflashメッセージの非表示を行えるため、コメントアウト
+// $(function() {
+//   $('.flash-message').fadeOut(4000);
+// });
+
+// JavaScriptで非表示にする場合（仮）
+// document.addEventListener("turbo:load", function() {
+//   const flashMessage = document.getElementsByClassName("flash-message");
+//   console.log(flashMessage);
+
+//   if(flashMessage){
+//     setTimeout(function() {
+//       flashMessage.classList.add("hide");
+//       // もしくは
+//       // flashMessage.style.display = "none";
+//     }, 4000);
+//   }
+// });
