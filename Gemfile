d07c3a61add05893a44cbf88e6f1e4e883918b30
kyohei-p <ysdkyh11@gmail.com>
2025-01-03 16:33:20 +0900

    add:Google Map Api導入

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ gem 'sassc'
 
 gem 'jquery-rails'
 
+gem 'geocoder'
+gem 'google_places'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   validates :email, uniqueness: true, presence: true
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
 end
